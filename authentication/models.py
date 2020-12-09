581529ccca3678fbcb1c01b7abc648a87f928662
REAGAN2020 <reaganteflon@gmail.com>
2020-12-09 23:44:36 +0300

    added a model class in authantication app

--- authentication/models.py
+++ authentication/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    USER_ROLES = (
+        ('CA', 'COMPANY ADMIN'),
+        ('NU', 'NORMAL USER'),
+        ('V', 'VENDOR')
+    )
+  
