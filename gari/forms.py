b8b6596dc40b31fcbb01c9d7a83c8db8c4579891
REAGAN2020 <reaganteflon@gmail.com>
2020-12-09 23:12:48 +0300

    inserted some functions in forms

---
+++ gari/forms.py
@@ -0,0 +1,106 @@
+from django.forms import ModelForm, Textarea
+from .models import *
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import User
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['username', 'rating', 'comment']
+        widgets = {
+            'comment': Textarea(attrs={'cols': 40, 'rows': 15})
+        }
+
+
+def should_be_empty(value):
+    if value:
+        raise forms.ValidationError('Field is not empty')
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=80)
+    message = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField()
+    forcefield = forms.CharField(
+        required=False, widget=forms.HiddenInput, label="Leave empty", validators=[should_be_empty])
+
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("email is taken")
+        return email
+    
+    def clean_username(self):
+        if self.is_valid():
+            username = self.cleaned_data['username']
+            try:
+                account = Account.objects.exclude(pk=self.instance.pk).get(username=username)
+            except Account.DoesNotExist:
+                return username
+            raise forms.ValidationError('Username "%s" is already in use.' % username)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+
+class UserAdminCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('email','username', 'password')
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
