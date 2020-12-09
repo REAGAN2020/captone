e68272d2cd74529a52f82ba764ba533c2d2a6a78
REAGAN2020 <reaganteflon@gmail.com>
2020-12-09 23:10:15 +0300

    added a class to review in models

--- gari/models.py
+++ gari/models.py
@@ -86,3 +86,34 @@ class Post(models.Model):
     @classmethod
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
+
+    
+class Comment(models.Model):
+    name = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+    
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),class Services(models.Model):
+    image = CloudinaryField('image', null=True)
+    description = TextField()
+    name =models.CharField(max_length=100)
+    prices=models.IntegerField()
+    
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+    
+    def average_rating(self):
+        all_ratings = map(lambda x: x.ratings, self.review_set.all())
+        return np.mean(all_ratings)
+          
